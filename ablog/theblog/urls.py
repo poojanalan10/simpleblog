b1337d8a68677486ef2fe5bbb00f607027ed3182
poojanalan10 <77766319+poojanalan10@users.noreply.github.com>
2023-11-25 14:19:18 -0800

    Model view and templates

---
+++ ablog/theblog/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from .views import HomeView,ArticleView
+
+urlpatterns = [
+  
+    #path('',views.home,name="home"),
+    path('',HomeView.as_view(),name="home"),
+    path('article/<int:pk>',ArticleView.as_view(),name="article"),
+]
---
+++ ablog/theblog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+class ArticleView(DetailView):
+    model = Post
+    template_name = 'article.html'
